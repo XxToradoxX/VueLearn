090e0376be7bd545400f65eae8c2dc02d77a3f59
PiPiGeee <1784264314@qq.com>
2024-05-23 14:18:35 +0800

    666

--- vite-butTs/src/main.ts
+++ vite-butTs/src/main.ts
@@ -1,5 +1,10 @@
 //ts好像不能用axiosVue引用
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from "axios";
+import axiosVue from "axios-vue";
+
+let app = createApp(App)
+app.use(axiosVue, axios)
 createApp(App).mount('#app')
